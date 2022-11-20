296aad566fe44d9014370e15747446d852327175
Vex1cK <118670080+Vex1cK@users.noreply.github.com>
2022-11-20 18:17:00 +0300

    Third commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def printer(a):
-    if a == 2:
+    if a == '2':
         print('This is second version')
+    elif a == 'BOB':
+        print('CAM TЫ BOB xDD')
     else:
         print("I don't know what is this version")
 
 
 if __name__ == '__main__':
-    printer(2)
+    a = input('Введите 2 либо BOB: ')
+    printer(a)
