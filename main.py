a1ebdda5d93873f2892cc78c9fdd11b7776a9b9c
Vex1cK <118670080+Vex1cK@users.noreply.github.com>
2022-11-20 18:02:40 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('This is firts version')
+def printer(a):
+    if a == 2:
+        print('This is second version')
+    else:
+        print("I don't know what is this version")
+
+
+if __name__ == '__main__':
+    printer(2)
